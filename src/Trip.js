b12a0a95b0b86158a789e1205a1fc52c964926e2
William Phelps <williamphelps13@gmail.com>
2021-08-09 10:19:50 -0600

    Add trip properties

--- src/Trip.js
+++ src/Trip.js
@@ -1,7 +1,18 @@
 import dayjs from 'dayjs';
 
 class Trip {
-  constructor() {
-
+  constructor(allDestinations) {
+    this.allDestinations = allDestinations;
+    this.destinationID = null;
+    this.date = null;
+    this.duration = null;
+    this.travelerCount = null;
+    this.status = 'pending';
+    this.tripCost = null;
+    this.agentFee = null;
+    this.totalCost = null;
   }
+  
 }
+
+export default Trip;
