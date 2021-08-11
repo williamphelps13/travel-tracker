fd5088dd1d84749d8c8e67791d22cc43cd4551a0
William Phelps <williamphelps13@gmail.com>
2021-08-10 18:55:17 -0600

    Move DOM updating code to DOMUpdates file

--- src/scripts.js
+++ src/scripts.js
@@ -1,5 +1,4 @@
 import './css/base.scss';
-import MicroModal from 'micromodal';
 import Glide from '@glidejs/glide';
 import dayjs from 'dayjs';
 import Traveler from './Traveler'
@@ -21,36 +20,44 @@ const config = {
   }
 };
 
-
-
-
 let tripRepo, traveler, trip;
 
-window.addEventListener('load', openModal);
+let {
+  username,
+  password,
+  destinationInput,
+  dateInput,
+  durationInput,
+  travelerCountInput,
+} = domUpdates;
 
-document.getElementById('openModalButton').addEventListener('click', openModal);
+window.addEventListener('load', displayModal);
+document.getElementById('openModalButton').addEventListener('click', displayModal);
 
-function openModal() {
-  MicroModal.show('login-modal');
-};
+// function displayHistoricalWeek(randomHistory) {
+//   domUpdates.renderHistoricalWeek(randomHistory);
+// }
+// const {
+//   hydrationButton,
+//   hydrationInput,
+//   hydrationHeader
+// } = domUpdates;
+
+function displayModal() {
+  domUpdates.renderModal()
+}
 
 document.getElementById('loginButton').addEventListener('click', getIDFromUsername)
 
 
 function getIDFromUsername() {
-  const username = document.getElementById('usernameInput');
-  const password = document.getElementById('passwordInput');
-
   traveler = new Traveler(username.value, password.value);
   // traveler = new Traveler('traveler44', 'travel');
   traveler.getIDFromUsername();
 
-  
-  if (password.value === "travel") {
-    document.querySelector('.login-page').classList.add("hidden");
-    document.querySelector('.user-page').classList.remove("hidden");
+  if (password.value === 'travel') {
+    domUpdates.renderHideLogin();
     new Glide('.glide', config).mount();
-
     distributeData();
   }
 }
@@ -66,6 +73,7 @@ function distributeData() {
     traveler.name = travelerData.name;
     traveler.travelerType = travelerData.travelerType;
     traveler.destinations = allDestinations;
+    traveler.travelerTrips = tripRepo.getTripsByID(traveler.id);
 
     trip = new Trip(allDestinations);
 
@@ -77,19 +85,17 @@ function retrieveData() {
 }
 
 function displayDashboard() {
-  traveler.travelerTrips = tripRepo.getTripsByID(traveler.id);
-  
   sortTrips();
+
   traveler.getTotalSpentThisYear();
+  domUpdates.renderTotalSpentThisYear(traveler);
 
-  addDestinations()
+  domUpdates.renderDestinations(trip);
 
-  document.getElementById('spent').innerText = traveler.totalSpentThisYear;
-
-  renderTrips(traveler.pastTrips, 'tripsGridPast')
-  renderTrips(traveler.currentOrNextTrip, 'tripsGridCurrent')
-  renderTrips(traveler.upcomingTrips, 'tripsGridUpcoming')
-  renderTrips(traveler.pendingTrips, 'tripsGridPending')
+  domUpdates.renderTrips(traveler.pastTrips, 'tripsGridPast', trip)
+  domUpdates.renderTrips(traveler.currentOrNextTrip, 'tripsGridCurrent', trip)
+  domUpdates.renderTrips(traveler.upcomingTrips, 'tripsGridUpcoming', trip)
+  domUpdates.renderTrips(traveler.pendingTrips, 'tripsGridPending', trip)
 }
 
 function sortTrips() {
@@ -97,40 +103,6 @@ function sortTrips() {
   traveler.getCurrentOrNextTrip()
   traveler.getUpcomingTrips()
   traveler.getPendingTrips()
-}
-
-function renderTrips(tripsList, section) {
-  const tripsGrid = document.getElementById(`${section}`);
-  tripsGrid.innerHTML = '';
-
-  tripsList.forEach(trips => {
-  let destinationName, imageURL, imageAlt;
-  
-  trip.allDestinations.forEach(destination => {
-    if (trips.destinationID === destination.id) {
-      destinationName = destination.destination;
-      imageURL = destination.image;
-      imageAlt= destination.alt;
-    }
-  })
-    tripsGrid.innerHTML +=
-   `<article class='trips'>
-      <div class='trips-image-container'>
-        <img class='trips-image' src=${imageURL} alt=${imageAlt}>
-      </div>
-      <p>Destination: ${destinationName}</p>
-      <p>Date: ${trips.date}</p>
-      <p>Duration: ${trips.duration}</p>
-      <p>Status: ${trips.status}</p>
-    </article>`
-  })
-}
-
-function addDestinations() {
-  trip.allDestinations.forEach(destination => {
-    destinationInput.innerHTML += `
-    <option value=${destination.id}>${destination.destination}</option>`
-  })
 }
 
 document.getElementById('estimateButton').addEventListener('click', displayEstimate)
@@ -145,21 +117,13 @@ function displayEstimate() {
   trip.getAgentFee()
   trip.getTotalCost()
 
-  document.getElementById('tripEstimate').innerText = trip.totalCost;
+  domUpdates.renderTripCost(trip);
 }
-
-let destinationInput = document.getElementById('destinationsDropdown');
-let dateInput = document.getElementById('startDate');
-let durationInput = document.getElementById('duration');
-let travelerCountInput = document.getElementById('party');
 
 function createTrip() {
   event.preventDefault();
 
-  destinationInput.value = "";
-  dateInput.value = "";
-  durationInput.value = "";
-  travelerCountInput.value = "";
+  domUpdates.clearInputs();
 
   let tripToPost = {
     id: tripRepo.allTrips.sort((a,b) => b.id - a.id)[0].id + 1,
@@ -176,7 +140,6 @@ function createTrip() {
 }
 
 function addInputsToTrip() {
-
   trip.destinationID = parseInt(destinationInput.value);
   trip.date = dateInput.value;
   trip.duration = parseInt(durationInput.value);
@@ -188,7 +151,6 @@ function postTrip(tripToPost) {
     if (!response.ok) {
       throw Error(response.statusText);
     } else {
-      
       distributeData();
       displayDashboard();
     }
